c146115b63f49e1a0be19794bea23201978aafb8
Gullit Moran <gullitmoran@hotmail.com>
2023-10-18 16:20:33 -0500

    deploy en render

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-//body-parse
 
+//body-parse
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Ruta
 
+// Ruta
 app.use('/', require('./routes/auth'));
+
 
 app.listen(process.env.PORT || 3000, () => {
 
     const port = process.env.PORT || 3000;
-    console.log(`Servidor corriendo en el puerto ${port}`);
+    console.log(`Servidor corriendo en el puerto.... ${port}`);
 
 })
